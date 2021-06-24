e6fa064edc9758ee1a64ad55302129ddab7be9dd
karla-walker <indiablonix@gmail.com>
2021-06-24 02:59:44 -0700

    Added error handlers

--- app.js
+++ app.js
@@ -28,7 +28,22 @@ app.get('/projects/:id', (req, res) => {
   res.render('project', { name });
 });
 
+// 404 ERROR HANDLER
+app.use((req, res, next) => {
+  const err = new Error("Uh oh, looks like this page doesn't exist.");
+  err.status = 404;
+  next(err);
+});
+
+// ERROR MIDDLEWARE
+app.use((err, req, res, next) => {
+  res.status(err.status);
+  console.log(err.message);
+  console.log(err.status);
+  next();
+});
+
 // SERVER SETUP
 app.listen(3000, () => {
-  console.log(`The app is runnin on localhost: 3000!`);
+  console.log(`The app is running on localhost: 3000!`);
 });
